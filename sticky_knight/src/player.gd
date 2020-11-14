eaa40a61074caef53d579f095f30fd39012ba8bf
zqf <zqf@zealousquakefan.com>
2020-11-14 16:40:07 +0000

    platformer - fixing is_on_floor detection

--- sticky_knight/src/player.gd
+++ sticky_knight/src/player.gd
@@ -5,13 +5,16 @@ const MAX_AIR_JUMPS = 1
 
 onready var _melee: PlayerMelee = $melee
 onready var _airJumpSprite: Sprite = $air_jump_sprite
+onready var _groundedSprite: Sprite = $grounded_sprite
 
 var _velocity = Vector2()
 var _gravity = Vector2(0, 900)
 var _lastMoveDir = Vector2(1, 0)
 var _airJumps:int = 1
+var _frame:int = 0
 
 func _physics_process(_delta):
+	_frame += 1
 	var pushX = 0.0
 	if Input.is_action_pressed("ui_left"):
 		pushX -= 1.0
@@ -20,17 +23,25 @@ func _physics_process(_delta):
 	_velocity.x = pushX * SPEED_X
 	
 	_airJumpSprite.visible = (_airJumps > 0)
+	_groundedSprite.visible = is_on_floor()
 	
 	if is_on_floor():
 		_airJumps = MAX_AIR_JUMPS
 		if Input.is_action_just_pressed("jump"):
+			print(str(_frame) + "Jump")
 			_velocity.y = -350
+			#_velocity.y = -500
 		pass
 	else:
 		if _airJumps > 0 && Input.is_action_just_pressed("jump"):
+			print(str(_frame) + "Air jump")
 			_velocity.y = -350
 			_airJumps -= 1
-		_velocity.y += _gravity.y * _delta
+	
+	# gravity must always be applied even when on ground
+	# otherwise is_on_floor will fail if move_and_slide
+	# has no y component into the floor!
+	_velocity.y += _gravity.y * _delta
 	
 	# update default face direction
 	if _velocity.x > 0:
