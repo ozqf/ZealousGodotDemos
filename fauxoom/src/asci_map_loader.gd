bcb2a575a01e6c74e006f4a1967d75e748b7e0b1
zqf <zqf@zealousquakefan.com>
2021-02-02 01:34:58 +0000

    Fauxoom - moving new map load function out of map def class

--- fauxoom/src/asci_map_loader.gd
+++ fauxoom/src/asci_map_loader.gd
@@ -1,6 +1,7 @@
 extends Node
 class_name AsciMapLoader
 const _map_t = preload("res://src/defs/map_def.gd")
+const _spawnDef_t = preload("res://src/defs/map_spawn_def.gd")
 
 const asci0:String = """#####
 #. .#
@@ -96,12 +97,95 @@ static func get_default() -> String:
 	#return asci2
 	#return asci1
 
+################################################
+# build MapDef from asci grid
+################################################
+
 static func build_test_map() -> void:
-	var m:MapDef = _map_t.new()
-	m.load_from_asci(asci4)
+	# var m:MapDef = _map_t.new()
+	# m.load_from_asci(asci4)
+	var m:MapDef = load_from_asci(asci4)
+	if m == null:
+		print("ERROR Failed to load map from asci")
+		return
 	print("build test map:")
 	print(m.debug_print_cells())
 	#return m
+
+static func char_to_tile_type(c:String) -> int:
+	if c == '#':
+		return MapDef.TILE_WALL
+	elif c == '.':
+		return MapDef.TILE_VOID
+	else:
+		return MapDef.TILE_PATH
+
+static func char_to_ent(c:String, x:int, y:int) -> MapSpawnDef:
+	var newType:int = MapDef.ENT_TYPE_NONE
+	if c == 'x':
+		# print("Spawn mob at " + str(x) + ", " + str(y))
+		newType = MapDef.ENT_TYPE_MOB_GRUNT
+	elif c == 's':
+		newType = MapDef.ENT_TYPE_START
+	elif c == 'e':
+		newType = MapDef.ENT_TYPE_END
+	elif c == 'k':
+		newType = MapDef.ENT_TYPE_KEY
+	
+	# didn't get anything?
+	if newType == MapDef.ENT_TYPE_NONE:
+		return null
+	
+	var spawn:MapSpawnDef = _spawnDef_t.new()
+	spawn.type = newType
+	spawn.position = Vector3(x, 0, y)
+	spawn.yaw = int(rand_range(0, 359))
+	return spawn
+
+static func load_from_asci(txt:String) -> MapDef:
+	
+	txt = txt.replace("\r", "")
+	print("Load test map from asci")
+	print(txt)
+	var lines:PoolStringArray = txt.split("\n", false)
+	# \n will break up each row, but assume row widths might be
+	# different and should be measured
+	var longest:int = 0
+	for i in range(0, lines.size()):
+		var length = lines[i].length()
+		if length > longest:
+			longest = length
+	if longest == 0 || lines.size() == 0:
+		print("Read 0 width or height from map asci")
+		return null
+	var newWidth:int = longest
+	var newHeight:int = lines.size()
+	var _newCells:PoolIntArray = []
+	var _newSpawns = []
+	for y in range(0, lines.size()):
+		var line = lines[y]
+		for x in range(0, line.length()):
+			var c:String = line[x]
+			var tileType:int = char_to_tile_type(c)
+			_newCells.push_back(tileType)
+			var spawn:MapSpawnDef = char_to_ent(c, x, y)
+			if spawn:
+				_newSpawns.push_back(spawn)
+		var pad:int = longest - line.length()
+		for _i in range(0, pad):
+			_newCells.push_back(MapDef.TILE_WALL)
+	
+	# loaded chucks successfully
+	var map:MapDef = MapDef.new()
+	map.width = newWidth
+	map.height = newHeight
+	map.cells = _newCells
+	map.spawns = _newSpawns
+	return map
+
+################################################
+# encoding scribbles
+################################################
 
 static func str_to_b64(source:String) -> String:
 	var srcBytes:PoolByteArray = source.to_utf8()
--- fauxoom/src/defs/map_def.gd
+++ fauxoom/src/defs/map_def.gd
@@ -1,6 +1,6 @@
 class_name MapDef
 
-var _spawnDef_t = preload("res://src/defs/map_spawn_def.gd")
+const _spawnDef_t = preload("res://src/defs/map_spawn_def.gd")
 
 const TILE_PATH:int = 0
 const TILE_WALL:int = 1
@@ -15,81 +15,24 @@ const ENT_TYPE_KEY:int = 4
 var name:String = "No Title"
 var width:int = 32
 var height:int = 32
-var _cells:PoolIntArray = []
-var _spawns = []
+var cells:PoolIntArray = []
+var spawns = []
 
 func is_pos_safe(x:int, y:int) -> bool:
 	return (x < 0 || x >= width || y < 0 || y >= height)
 
 func get_type_at(x:int, y:int) -> int:
-	return _cells[x + (y * width)]
-
-func _char_to_tile_type(c:String) -> int:
-	if c == '#':
-		return TILE_WALL
-	elif c == '.':
-		return TILE_VOID
-	else:
-		return TILE_PATH
-
-func _char_to_ent(c:String, x:int, y:int) -> int:
-	var newType:int = ENT_TYPE_NONE
-	if c == 'x':
-		print("Spawn mob at " + str(x) + ", " + str(y))
-		newType = ENT_TYPE_MOB_GRUNT
-	elif c == 's':
-		newType = ENT_TYPE_START
-	elif c == 'e':
-		newType = ENT_TYPE_END
-	elif c == 'k':
-		newType = ENT_TYPE_KEY
-	
-	if newType == ENT_TYPE_NON:
-		return ENT_TYPE_NONE
-	var spawn:MapSpawnDef = _spawnDef_t.new()
-	spawn.type = newType
-	spawn.position = Vector(x, 0, y)
-	spawn.yaw = rand_range(0, 359)
-	_spawns.push_back(spawn)
-	return newType
+	return cells[x + (y * width)]
 
 func debug_print_cells() -> String:
-	var result:String = "Map " + str(width) + ", " + str(height)
-	result += " ents: " + str(_spawns.size()) + "\n"
-	var _len = _cells.size()
+	var result:String = "Map (" + str(width) + " by " + str(height) + ")"
+	result += " ents: " + str(spawns.size()) + "\n"
+	var _len = cells.size()
 	for i in range (0, _len):
-		result += str(_cells[i])
+		result += str(cells[i])
 		if (i + 1) % width == 0:
 			result += '\n'
+	for i in range (0, spawns.size()):
+		var spawn:MapSpawnDef = spawns[i]
+		result += "Ent type " + str(spawn.type) + " at pos " + str(spawn.position.x) + ", " + str(spawn.position.y) + "\n"
 	return result
-
-func load_from_asci(txt:String) -> bool:
-	txt = txt.replace("\r", "")
-	print("Load test map from asci")
-	print(txt)
-	var lines:PoolStringArray = txt.split("\n", false)
-	# \n will break up each row, but assume row widths might be
-	# different and should be measured
-	var longest:int = 0
-	for i in range(0, lines.size()):
-		var length = lines[i].length()
-		if length > longest:
-			longest = length
-	if longest == 0 || lines.size() == 0:
-		print("Read 0 width or height from map asci")
-		return false
-	width = longest
-	height = lines.size()
-	var _newCells:PoolIntArray = []
-	for y in range(0, lines.size()):
-		var line = lines[y]
-		for x in range(0, line.length()):
-			var c:String = line[x]
-			var tileType:int = _char_to_tile_type(c)
-			_newCells.push_back(tileType)
-			_char_to_ent(c, x, y)
-		var pad:int = longest - line.length()
-		for _i in range(0, pad):
-			_newCells.push_back(TILE_WALL)
-	_cells = _newCells
-	return true
