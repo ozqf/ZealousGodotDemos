3e4f13be60a509dbf6004d3094b3567b78a1c936
zqf <zqf@zealousquakefan.com>
2021-03-17 23:49:43 +0000

    Rug - added more test meshes

--- enchanted_rug/src/projectile.gd
+++ enchanted_rug/src/projectile.gd
@@ -2,14 +2,24 @@ extends KinematicBody
 
 var _speed:float = 125.0
 var _ttl:float = 10
+var _dead:bool = false
+
+func remove() -> void:
+	if _dead:
+		return
+	_dead = true
+	queue_free()
+	
 
 func _physics_process(delta) -> void:
 	_ttl -= delta
 	if _ttl <= 0:
-		queue_free()
+		remove()
 		return
 	var move:Vector3 = (-global_transform.basis.z * _speed) * delta
-	move_and_collide(move)
+	var hit = move_and_collide(move)
+	if hit != null:
+		remove()
 
 func launch(pos:Vector3, dir:Vector3) -> void:
 	var t:Transform = Transform.IDENTITY
