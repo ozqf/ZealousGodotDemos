69034b8657d3f1922462384e614eb4903af01697
zqf <zqf@zealousquakefan.com>
2021-03-18 18:33:52 +0000

    Rug - further experiments with expanding column projectiles

---
+++ enchanted_rug/src/column_projectile.gd
@@ -0,0 +1,91 @@
+extends Spatial
+
+onready var _floor:RayCast = $floor
+onready var _ceiling:RayCast = $ceiling
+
+export var speed:float = 1
+export var minSpeed:float = 1
+export var maxSpeed:float = 100
+export var acceleration:float = 25
+export var spinRateDegrees:float = 22.5
+
+var _ttl:float = 10
+var _dead:bool = false
+
+var _growthRate:float = 25
+var _currentFloorScale:float = 1
+var _currentCeilingScale:float = 1
+
+func remove() -> void:
+	if _dead:
+		return
+	_dead = true
+	queue_free()
+
+func _physics_process(delta) -> void:
+	_ttl -= delta
+	if _ttl <= 0:
+		remove()
+		return
+	
+	# adjust speed
+	speed += acceleration * delta
+	if speed > maxSpeed:
+		speed = maxSpeed
+	if speed < minSpeed:
+		speed = minSpeed
+	
+	var move:Vector3 = (-global_transform.basis.z * speed) * delta
+	var t:Transform = global_transform
+	t.origin += move
+	global_transform = t
+	var degrees = rotation_degrees
+	degrees.z += spinRateDegrees * delta
+	rotation_degrees = degrees
+	# var hit = move_and_collide(move)
+	# if hit != null:
+	# 	remove()
+	# 	return
+	
+	# adjust size
+	var origin:Vector3 = global_transform.origin
+	var floorScale:float = 100
+	var ceilingScale:float = 100
+
+	if _floor.is_colliding():
+		floorScale = origin.distance_to(_floor.get_collision_point())
+		# var floorPos:Vector3 = _floor.get_collision_point()
+		# floorScale = abs(origin.y - floorPos.y)
+	
+	if _currentFloorScale < floorScale:
+		_currentFloorScale += _growthRate * delta
+	if _currentFloorScale > floorScale:
+		_currentFloorScale = floorScale
+	
+	$down.scale = Vector3(1, _currentFloorScale, 1)
+	
+	if _ceiling.is_colliding():
+		ceilingScale = origin.distance_to(_ceiling.get_collision_point())
+		# var ceilingPos:Vector3 = _ceiling.get_collision_point()
+		# ceilingScale = abs(ceilingPos.y + origin.y)
+	
+	if _currentCeilingScale < ceilingScale:
+		_currentCeilingScale += _growthRate * delta
+	if _currentCeilingScale > ceilingScale:
+		_currentCeilingScale = ceilingScale
+	$up.scale = Vector3(1, _currentCeilingScale, 1)
+
+func launch(pos:Vector3, dir:Vector3, _spinStartDegrees:float = 0, _spinRateDegrees:float = 0) -> void:
+	var t:Transform = global_transform
+	t.origin = pos
+	global_transform = t
+
+	var flatDir:Vector3 = Vector3(dir.x, 0, dir.z)
+	flatDir = flatDir.normalized()
+	look_at(pos + flatDir, Vector3.UP)
+	
+	var rot:Vector3 = rotation_degrees
+	rot.z = _spinStartDegrees
+	rotation_degrees = rot
+	spinRateDegrees = _spinRateDegrees
+
--- enchanted_rug/src/turret_attack.gd
+++ enchanted_rug/src/turret_attack.gd
@@ -1,17 +1,30 @@
 extends Spatial
 
 var _projectile_t = preload("res://prefabs/projectiles/prj_ball_large.tscn")
+var _column_projectile_t = preload("res://prefabs/projectiles/prj_column.tscn")
 
 var _active:bool = true
 
 var _refireTick:float = 0
+var _spinDir:float = 1
+export var refireRate:float = 1
 
-func _fire() -> void:
+func _ready() -> void:
+	add_to_group(Console.GROUP)
+
+func _fire(forward:Vector3, _spinStart:float = 0, _spinRate:float = 0) -> void:
 	var pos:Vector3 = global_transform.origin
-	var forward:Vector3 = -global_transform.basis.z
-	var prj = _projectile_t.instance()
+
+	# var prj = _projectile_t.instance()
+	var prj = _column_projectile_t.instance()
+
 	get_tree().get_current_scene().add_child(prj)
-	prj.launch(pos, forward)
+	prj.launch(pos, forward, _spinStart, _spinRate)
+
+func _fire_offset(spreadX:float, spreadY:float, _spinStart:float = 0, _spinRate:float = 0) -> void:
+	var t:Transform = global_transform
+	var offset:Vector3 = ZqfUtils.calc_forward_spread_from_basis(t.origin, t.basis, spreadX, spreadY)
+	_fire(offset)
 
 func _process(_delta:float) -> void:
 	if !_active:
@@ -21,8 +34,23 @@ func _process(_delta:float) -> void:
 		return
 	look_at(tar.position, Vector3.UP)
 
+	var t:Transform = global_transform
+	var forward:Vector3 = -t.basis.z
+
 	if _refireTick <= 0:
-		_refireTick = 0.1
-		_fire()
+		_spinDir *= -1
+		_refireTick = refireRate
+		_fire(forward, 0, 22.5 * _spinDir)
+		_fire(forward, 45, 22.5 * _spinDir)
+		_fire(forward, 90, 22.5 * _spinDir)
+		_fire(forward, 135, 22.5 * _spinDir)
+		
+		# _fire_offset(-5000, 0)
+		# _fire_offset(5000, 0)
+		# var offset:Vector3 = ZqfUtils.calc_forward_spread_from_basis(t.origin, t.basis, 5000, 0)
+		# _fire(offset)
+
+		# offset = ZqfUtils.calc_forward_spread_from_basis(t.origin, t.basis, -5000, 0)
+		# _fire(offset)
 	else:
 		_refireTick -= _delta
