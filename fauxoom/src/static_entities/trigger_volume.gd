1ee709d9acd97929f23923b6464d0d147f291063
zqf <zqf@zealousquakefan.com>
2021-04-11 20:45:50 +0100

    Fauxoom - trigger volume saves state again

--- fauxoom/src/static_entities/trigger_volume.gd
+++ fauxoom/src/static_entities/trigger_volume.gd
@@ -1,38 +1,30 @@
 extends Spatial
 
+onready var _ent:Entity = $Entity
+
 export var selfName:String = ""
 export var triggerTargetName:String = ""
 export var active:bool = true
 
-var _spawnState:Dictionary = {}
-
 func _ready() -> void:
 	visible = false
-	add_to_group(Groups.ENTS_GROUP_NAME)
 	var _result = self.connect("body_entered", self, "_on_body_entered")
-	add_to_group(Groups.GAME_GROUP_NAME)
-	_spawnState = write_state()
+	_result = _ent.connect("entity_append_state", self, "append_state")
+	_result = _ent.connect("entity_restore_state", self, "restore_state")
+	_result = _ent.connect("entity_trigger", self, "on_trigger")
 
-func write_state() -> Dictionary:
-	return {
-		selfName = selfName,
-		triggerTargetName = triggerTargetName,
-		active = active
-	}
+func append_state(_dict:Dictionary) -> void:
+	_dict.selfName = selfName
+	_dict.triggerTargetName = triggerTargetName
+	_dict.active = active
 
 func restore_state(data:Dictionary) -> void:
 	selfName = data.selfName
 	triggerTargetName = data.triggerTargetName
 	active = data.active
 
-func game_on_reset() -> void:
-	restore_state(_spawnState)
-
-func on_trigger_entities(target:String) -> void:
-	if target == "":
-		return
-	if target == selfName:
-		active = !active
+func on_trigger() -> void:
+	active = !active
 
 func _on_body_entered(_body:PhysicsBody) -> void:
 	if !active:
