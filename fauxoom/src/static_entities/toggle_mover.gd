5681a516744d0fa2c53d0cd9e06744fb579a3fdf
zqf <zqf@zealousquakefan.com>
2021-04-13 20:18:54 +0100

    Fauxoom - quick additions to toggle mover

--- fauxoom/src/static_entities/toggle_mover.gd
+++ fauxoom/src/static_entities/toggle_mover.gd
@@ -13,6 +13,18 @@ var _dir:float = 1
 func _ready():
 	_a.visible = false
 	_b.visible = false
+	var _r = _ent.connect("entity_append_state", self, "append_state")
+	_r = _ent.connect("entity_restore_state", self, "restore_state")
+	_r = _ent.connect("entity_trigger", self, "on_trigger")
+
+func append_state(_dict:Dictionary) -> void:
+	_dict.xform = ZqfUtils.transform_to_dict(_root.global_transform)
+
+func restore_state(_dict:Dictionary) -> void:
+	_root.global_transform = ZqfUtils.transform_from_dict(_dict.xform)
+
+func on_trigger() -> void:
+	pass
 
 func _process(delta) -> void:
 	_time += delta * _dir
