0cf2ead7f9ac291597a25ef91cd78b03eb8c4562
zqf <zqf@zealousquakefan.com>
2021-11-15 01:25:50 +0000

    Fauxoom - linking up second test arena spawn

--- fauxoom/src/static_entities/survival_controller.gd
+++ fauxoom/src/static_entities/survival_controller.gd
@@ -1,8 +1,8 @@
 extends Spatial
 
 onready var _ent:Entity = $Entity
-onready var _punk_spawner = $horde_spawns/punk_spawn
-onready var _worm_spawner = $horde_spawns/worm_spawn
+#onready var _punk_spawner = $horde_spawns/punk_spawn
+#onready var _worm_spawner = $horde_spawns/worm_spawn
 
 export var selfName:String = ""
 export var triggerTargetName:String = ""
@@ -12,6 +12,7 @@ var _active:bool = false
 
 var _spawnTransforms = []
 var _northernTransforms = []
+var _spawners = []
 
 func _ready() -> void:
 	visible = false
@@ -21,23 +22,48 @@ func _ready() -> void:
 	_r = _ent.connect("entity_trigger", self, "on_trigger")
 	_ent.selfName = selfName
 
-	var spawnPoints:Spatial = get_parent().get_node("arena_spawn_points")
+	var allTransforms = []
+	var spawnPoints:Spatial = get_parent().get_node(spawnPointsSiblingName)
 	var numPoints:int = spawnPoints.get_child_count()
 	for _i in range(0, numPoints):
-		_spawnTransforms.push_back(spawnPoints.get_child(_i).global_transform)
-
-	var cardinalTransforms = []
-	cardinalTransforms.push_back(spawnPoints.get_node("n").global_transform)
-	cardinalTransforms.push_back(spawnPoints.get_node("s").global_transform)
-	cardinalTransforms.push_back(spawnPoints.get_node("w").global_transform)
-	cardinalTransforms.push_back(spawnPoints.get_node("e").global_transform)
+		var child = spawnPoints.get_child(_i)
+		_spawnTransforms.push_back(child.global_transform)
+		allTransforms.push_back(child.global_transform)
 	
-	_punk_spawner.set_spawn_points(cardinalTransforms)
-	_worm_spawner.set_spawn_points(cardinalTransforms)
+	var useSubset:bool = false
+	var cardinalTransforms = []
+	#cardinalTransforms.push_back(spawnPoints.get_node("n").global_transform)
+	#cardinalTransforms.push_back(spawnPoints.get_node("s").global_transform)
+	#cardinalTransforms.push_back(spawnPoints.get_node("w").global_transform)
+	#cardinalTransforms.push_back(spawnPoints.get_node("e").global_transform)
+	
+	var spawnsParent = $horde_spawns
+	var numSpawners:int = spawnsParent.get_child_count()
+	for _i in range(0, numSpawners):
+		_spawners.push_back(spawnsParent.get_child(_i))
+	
+	if useSubset:
+		set_all_spawner_destinations(cardinalTransforms)
+#		_punk_spawner.set_spawn_points(cardinalTransforms)
+#		_worm_spawner.set_spawn_points(cardinalTransforms)
+	else:
+		set_all_spawner_destinations(allTransforms)
+#		_punk_spawner.set_spawn_points(cardinalTransforms)
+#		_worm_spawner.set_spawn_points(cardinalTransforms)
 
 func _process(_delta:float) -> void:
 	if !_active:
 		return
+
+func set_all_spawner_destinations(transformsArray) -> void:
+	for i in range(0, _spawners.size()):
+		if !_spawners[i].has_method("set_spawn_points"):
+			print("node " + str(_spawners[i].name) + " has no set spawn points function")
+		_spawners[i].set_spawn_points(transformsArray)
+
+func start_wave_spawners() -> void:
+	for i in range(0, _spawners.size()):
+		_spawners[i].on_trigger()
 
 func on_trigger() -> void:
 	_active = true
@@ -45,8 +71,9 @@ func on_trigger() -> void:
 	# _punk_spawner.tickMax = 0.25
 	# _punk_spawner.totalMobs = 9999
 	# _punk_spawner.maxLiveMobs = 10
-	_punk_spawner.on_trigger()
-	_worm_spawner.on_trigger()
+	start_wave_spawners()
+#	_punk_spawner.on_trigger()
+#	_worm_spawner.on_trigger()
 
 func append_state(_dict:Dictionary) -> void:
 	_dict.active = _active
