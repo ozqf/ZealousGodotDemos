d3c559b510d16dcaf8e07720a64263244aabf22f
zqf <zqf@zealousquakefan.com>
2022-03-12 23:04:12 +0000

    Fauxoom - adding sin wave wiggle to boss tentacles

--- fauxoom/src/bosses/smatter_limb.gd
+++ fauxoom/src/bosses/smatter_limb.gd
@@ -8,29 +8,56 @@ onready var _nodes:Spatial = $nodes
 export var length:int = 8
 var _moveWeight:float = 0.0
 
+var _time:float = 0.0
+
 func _ready():
 	if length <= 0:
 		length = 8
-	for _i in range(0, length * 2):
+	for _i in range(0, length * 4):
 		var segment = _limb_segment_t.instance()
 		_nodes.add_child(segment)
-	_animate(-1)
+	_animate(0)
 	_position_nodes()
 
+func _calc_limit_promxity(count:float, total:float) -> float:
+	var half:float = total * 0.5
+	if count > half:
+		count = total - count
+	return count / half
+
 func _position_nodes() -> void:
+	# in local coordinates:
+
 	var origin:Vector3 = Vector3() # transform.origin
 	var dest:Vector3 = _tip.transform.origin
+	var left:Vector3 = _tip.transform.basis.x;
 	#print("Limb from " + str(origin) + " to " + str(dest))
 	var numSegments:int = _nodes.get_child_count()
+	
+	# offsetting wave
+	var frequency:float = 4
+	var offsetStep:float = PI / (float(numSegments) * (1.0 / frequency))
+	
+	var time:float = _time - floor(_time)
+	var offsetOrigin:float = float(numSegments) * time
+	#print("Offset origin: " + str(offsetOrigin))
+	
 	for i in range (0, numSegments):
+		var segment = _nodes.get_child(i)
 		var weight:float = float(i) / float(numSegments)
 		var pos:Vector3 = origin.linear_interpolate(dest, weight)
-		var segment = _nodes.get_child(i)
+		var offsetWeight:float = cos((float(i) + offsetOrigin) * offsetStep)
+		offsetWeight *= _calc_limit_promxity(i, numSegments)
+		var offset:Vector3 = (left * offsetWeight)
+		pos += offset
+		
 		segment.transform.origin = pos
+
 	pass
 
 func _animate(delta:float) -> void:
-	_moveWeight += (delta * 2.0)
+	#_moveWeight += (delta * 2.0)
+	_moveWeight = 0.5
 	var weight:float = sin(_moveWeight)
 	var offset:float = weight * 5.0
 	var up:Vector3 = transform.basis.y
@@ -47,6 +74,7 @@ func _animate(delta:float) -> void:
 	pass
 
 func _process(delta):
+	_time += (delta * 0.25)
 	_animate(delta)
 	_position_nodes()
 	pass
