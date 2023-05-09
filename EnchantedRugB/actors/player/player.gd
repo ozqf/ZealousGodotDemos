300475b8f2a4cd4e5ef860065824b7d96c7d6428
zqf <zqf@zealousquakefan.com>
2023-05-09 21:58:44 +0100

    ERB - mouse input

--- EnchantedRugB/actors/player/player.gd
+++ EnchantedRugB/actors/player/player.gd
@@ -1,14 +1,15 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
+const SPEED = 10.0
 const JUMP_VELOCITY = 4.5
+
+@onready var _head:Node3D = $head
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
-func _physics_process(delta):
+func _physics_process_default(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -30,5 +31,30 @@ func _physics_process(delta):
 
 	move_and_slide()
 
+
+func _physics_process(_delta:float) -> void:
+	var input_dir:Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	
+	if Zqf.has_mouse_claims():
+		input_dir = Vector2()
+	var direction:Vector3 = (_head.global_transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	move_and_slide()
+
 func teleport(t:Transform3D) -> void:
 	self.global_position = t.origin
+
+func _input(event) -> void:
+	if Zqf.has_mouse_claims():
+		return
+	var motion:InputEventMouseMotion = event as InputEventMouseMotion
+	if motion == null:
+		return
+	var degrees:Vector3 = _head.rotation_degrees
+	degrees.y += (-motion.relative.x) * 0.1
+	_head.rotation_degrees = degrees
--- EnchantedRugB/zqf/zqf_main.gd
+++ EnchantedRugB/zqf/zqf_main.gd
@@ -1,8 +1,52 @@
 extends Node
 class_name ZqfMain
 
+var _playerInputOn:bool = false
+
 func _ready():
+	set_player_input_on(false)
+
+func _process(_delta):
+	if Input.is_action_just_pressed("toggle_console"):
+		set_player_input_on(!_playerInputOn)
 	pass
 
-func _process(delta):
-	pass
+func set_player_input_on(flag:bool) -> void:
+	_playerInputOn = flag
+	if _playerInputOn:
+		remove_mouse_claim(self)
+	else:
+		add_mouse_claim(self)
+#	if _playerInputOn:
+#		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+#	else:
+#		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+func get_player_input_on() -> bool:
+	return _playerInputOn
+
+###################################################################
+# Mouse claims
+###################################################################
+var _mouseClaims:Array = []
+
+func _refresh_mouse_claims() -> void:
+	if _mouseClaims.size() > 0:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	else:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func has_mouse_claims() -> bool:
+	return _mouseClaims.size() > 0
+
+func add_mouse_claim(owner:Node) -> void:
+	var i:int = _mouseClaims.find(owner)
+	if i == -1:
+		_mouseClaims.push_back(owner)
+	_refresh_mouse_claims()
+
+func remove_mouse_claim(owner:Node) -> void:
+	var i:int = _mouseClaims.find(owner)
+	if i >= 0:
+		_mouseClaims.remove_at(i)
+	_refresh_mouse_claims()
