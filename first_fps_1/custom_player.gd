7e963f68cf902e25d824d3fddaaea7f08a0d086b
zqf <zqf@zealousquakefan.com>
2023-11-23 12:55:02 +0000

    Cleaning up some comments

--- first_fps_1/custom_player.gd
+++ first_fps_1/custom_player.gd
@@ -23,6 +23,9 @@ func _process(delta):
 	
 	# horizontal movement (X and Z)
 	# translate input axes to axes of object
+	# Basis is a 3x3 matrix storing x/y/z orientation vectors.
+	# we multiply the forward and left of our current orientation
+	# to scale them by the input
 	var rot:Basis = global_transform.basis
 	var pushDir:Vector3 = Vector3()
 	pushDir += inputDir.x * rot.x
@@ -31,14 +34,16 @@ func _process(delta):
 	
 	# separate out horizontal component of velocity, calculate it
 	# and apply it back. Y is handled independently
-	var horizontal:Vector3 = velocity
+	var horizontal:Vector3 = self.velocity
 	horizontal.y = 0
 	
-	# friction if the player is not pushing, or their push i
+	# apply friction to slow teh player if required.
+	# this is a straight multiply with no delta and thus framerate dependent
 	if pushDir == Vector3.ZERO:
 		if is_on_floor():
 			horizontal = horizontal * FRICTION_FLOOR
 	else:
+		# apply current input push
 		var pushStrength:float = ACCELERATION_GROUND
 		if !is_on_floor():
 			pushStrength = ACCELERATION_AIR
@@ -48,15 +53,16 @@ func _process(delta):
 	horizontal = horizontal.limit_length(RUN_SPEED)
 	
 	# apply new hoizontal motion back to velocity
-	velocity.x = horizontal.x
-	velocity.z = horizontal.z
+	self.velocity.x = horizontal.x
+	self.velocity.z = horizontal.z
 	
 	# vertical
 	if is_on_floor():
 		if inputDir.y > 0:
-			velocity.y = 10.0
+			self.velocity.y = 10.0
 	else:
-		velocity += Vector3(0, -_gravity, 0) * delta
+		# +y is up so gravity strength is negative when applied. 
+		self.velocity += Vector3(0, -_gravity, 0) * delta
 	
 	# do
 	move_and_slide()
@@ -66,6 +72,9 @@ func _input(event):
 	if motion == null:
 		return
 	
+	# motion.relative is movement from last cursor position.
+	# value is in pixels and thus not resolution dependent!
+	# sensitivity will go down as resolution goes up here.
 	var yawRadians:float = -motion.relative.x * MOUSE_RELATIVE_SENSITIVITY
 	self.rotate(Vector3.UP, yawRadians)
 	
