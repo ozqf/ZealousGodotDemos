03d9dda2c1fafc0f880ebf5970c47e359b533098
ozqf <zqf@zealousquakefan.com>
2025-02-05 00:26:00 +0000

    GS - playing with juggle / bounce logic

--- GhastlyShank/actors/mobs/stress_cube.gd
+++ GhastlyShank/actors/mobs/stress_cube.gd
@@ -7,6 +7,9 @@ var _stateTick:float = 0.0
 var _stateTime:float = 0.0
 var _weightClass:int = Mobs.WEIGHT_CLASS_FODDER
 
+var _gravityStrengthWeight:float = 1.0
+var _gravityStrengthDelayTime:float = 1.0
+
 func hit(_incomingHit:HitInfo) -> int:
 	print("Stress cube hit")
 	if _incomingHit.launchStrength > 0.0:
@@ -15,21 +18,32 @@ func hit(_incomingHit:HitInfo) -> int:
 		if _state != Mobs.STATE_LAUNCHED:
 			begin_launch(_incomingHit.launchYawRadians, _incomingHit.teamId)
 	elif _state == Mobs.STATE_JUGGLED:
-		begin_juggle(4.0)
+		begin_suspend(0.5)
 	elif _incomingHit.juggleStrength > 0.0:
 		#print("Juggled!")
-		begin_juggle(10.0)
+		begin_juggle(_incomingHit.juggleStrength)
 	return 1
 
 func begin_juggle(strength:float = 10.0) -> void:
 	_state = Mobs.STATE_JUGGLED
-	self.velocity.y = strength
-	#self.velocity = Vector3(0, strength, 0)
+	#self.velocity.y = strength
+	#if strength < 1.0:
+	#	_gravityStrengthWeight = 0.0
+	self.velocity = Vector3(0, strength, 0)
+
+func begin_suspend(strength:float = 1.0) -> void:
+	_state = Mobs.STATE_JUGGLED
+	#self.velocity.y = strength
+	if strength < 1.0:
+		_gravityStrengthWeight = 0.0
+	self.velocity = Vector3(0, strength, 0)
 
 func begin_fallen() -> void:
+	_stateTick = 1.0
 	_state = Mobs.STATE_FALLEN
 
 func begin_rising() -> void:
+	_stateTick = 1.0
 	_state = Mobs.STATE_NEUTRAL
 
 func begin_launch(_yaw:float, _launchingTeamId:int = 0) -> void:
@@ -47,6 +61,9 @@ func begin_launch(_yaw:float, _launchingTeamId:int = 0) -> void:
 
 const GRAVITY_MPS:float = 10.0
 
+func calc_gravity() -> float:
+	return GRAVITY_MPS * _gravityStrengthWeight
+
 func _refresh_debug_text() -> void:
 	var stateName:String = "neutral"
 	match _state:
@@ -58,21 +75,27 @@ func _refresh_debug_text() -> void:
 			stateName = "rising"
 		Mobs.STATE_LAUNCHED:
 			stateName = "launched"
-	_debugText.text = stateName
+	var txt:String = stateName
+	txt += "\nTick " + str(_stateTick)
+	_debugText.text = txt
 
 func _physics_process(_delta: float) -> void:
 	_refresh_debug_text()
+	if _gravityStrengthWeight < 1.0:
+		_gravityStrengthWeight += _gravityStrengthDelayTime * _delta
+		if _gravityStrengthWeight > 1.0:
+			_gravityStrengthWeight = 1.0
 	match _state:
 		Mobs.STATE_NEUTRAL:
 			var drag:Vector3 = ZqfUtils.calc_drag_vector3(self.velocity, 5.0, _delta)
 			self.velocity += drag
-			self.velocity.y += -GRAVITY_MPS * _delta
+			self.velocity.y += -calc_gravity() * _delta
 			self.move_and_slide()
 		Mobs.STATE_JUGGLED:
 			if self.is_on_floor() && self.velocity.y <= 0.0:
 				begin_fallen()
 				return
-			self.velocity.y += -GRAVITY_MPS * _delta
+			self.velocity.y += -calc_gravity() * _delta
 			self.move_and_slide()
 		Mobs.STATE_FALLEN:
 			if !self.is_on_floor():
@@ -80,17 +103,20 @@ func _physics_process(_delta: float) -> void:
 				return
 			_stateTick -= _delta
 			if _stateTick <= 0.0:
-				_stateTick = 999
 				begin_rising()
+			else:
+				var drag:Vector3 = ZqfUtils.calc_drag_vector3(self.velocity, 5.0, _delta)
+				self.velocity += drag
+				self.velocity.y += -calc_gravity() * _delta
+				self.move_and_slide()
 		Mobs.STATE_RISING:
 			_stateTick -= _delta
 			if _stateTick <= 0.0:
-				_stateTick = 999
+				_stateTick = _stateTime
 				_state = Mobs.STATE_NEUTRAL
 		Mobs.STATE_LAUNCHED:
 			_stateTick -= _delta
 			if _stateTick <= 0.0:
-				_stateTick = 999
 				begin_fallen()
 				return
 			
@@ -102,6 +128,7 @@ func _physics_process(_delta: float) -> void:
 					if "collision_layer" in obj:
 						var layer:int = obj.collision_layer
 						if (layer & (1 << 0)) == 1:
+							begin_juggle(3.5)
 							var n:Vector3 = results.get_normal(i)
 							n *= 5.0
 							# force pop up
@@ -109,6 +136,5 @@ func _physics_process(_delta: float) -> void:
 							n.y = 5.0
 							self.velocity = n
 							print("Begin fall, vel " + str(n))
-							begin_juggle(1.5)
 							break;
 				return
