31cfd6f787e1830189bf326af9f4410c4d8caf9f
ozqf <zqf@zealousquakefan.com>
2025-06-29 14:29:37 +0100

    LTT - added some more quick WIP test movement modes

--- carry_the_torch/actors/player/avatar_type_b/player_avatar_type_b.gd
+++ carry_the_torch/actors/player/avatar_type_b/player_avatar_type_b.gd
@@ -17,9 +17,16 @@ func _process(_delta: float) -> void:
 func _physics_process(_delta) -> void:
 
 	var t:Transform3D = self.global_transform
-
-	var pushDir:Vector3 = ZqfUtils.input_to_push_vector_flat(_input.inputDir, t.basis)
+	
+	# var _pushDirection:Vector3 = (basis * Vector3(inputDir.x, 0, inputDir.y)).normalized()
+	#var pushDir:Vector3 = ZqfUtils.input_to_push_vector_flat(_input.inputDir, t.basis)
+	var pushInput:Vector3 = Vector3(_input.inputDir.x, 0, _input.inputDir.y)
+	var pushDir:Vector3 = (t.basis * pushInput).normalized()
 	pushDir *= 10.0
-	self.velocity = pushDir
+	var v:Vector3 = self.velocity
+	v.x = pushDir.x
+	v.y += -20.0 * _delta
+	v.z = pushDir.z
+	self.velocity = v
 	self.move_and_slide()
 	pass
--- carry_the_torch/actors/player/start/player_start.gd
+++ carry_the_torch/actors/player/start/player_start.gd
@@ -2,10 +2,20 @@ extends Node3D
 
 var _playerAvatarAttempt1 = preload("res://actors/player/player_avatar.tscn")
 var _playerAvatarAttempt2 = preload("res://actors/player/avatar_type_b/player_avatar_type_b.tscn")
+var _playerAvatarRoller1 = preload("res://actors/player/avatar_type_roller/player_avatar_roller.tscn")
+var _playerAvatarWheel1 = preload("res://actors/player/avatar_wheel_1/player_avatar_wheel_1.tscn")
 
 var _currentAvatar:Node3D = null
 
 @onready var _ui:Control = $ui
+
+func _spawn_prefab(prefab) -> void:
+	_currentAvatar = prefab.instantiate()
+	self.get_parent().add_child(_currentAvatar)
+	var t:Transform3D = self.global_transform
+	t.origin.y += 0.5
+	_currentAvatar.global_transform = t
+	self.queue_free()
 
 func _physics_process(_delta: float) -> void:
 	if _currentAvatar != null:
@@ -24,3 +34,7 @@ func _physics_process(_delta: float) -> void:
 		t.origin.y += 0.5
 		_currentAvatar.global_transform = t
 		self.queue_free()
+	elif Input.is_action_just_pressed("slot_3"):
+		_spawn_prefab(_playerAvatarRoller1)
+	elif Input.is_action_just_pressed("slot_4"):
+		_spawn_prefab(_playerAvatarWheel1)
